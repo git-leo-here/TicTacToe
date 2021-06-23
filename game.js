477c18b248ce8624dfb81079c52fea6a5f236da9
Leo <neotheone458@gmail.com>
2021-06-23 21:08:55 +0530

    first working version

--- game.js
+++ game.js
@@ -32,57 +32,62 @@ function play(e){
         }
     let row=Math.floor(position/3) //floor division by 3
     let column=(position%3)
-    e.target.innerHTML=`${g.current_player.char}`
-    e.target.style.color=g.current_player.clr
-    board[row][column]=g.current_player.char
-    
-    
-    if (checkWinner()){
-        console.log(checkWinner())
-        document.getElementById('currentPlayer').innerHTML=`Player ${g.current_player.char} won`
+    if (board[row][column]===0){
+        e.target.innerHTML=`${g.current_player.char}`
+        e.target.style.color=g.current_player.clr
+        board[row][column]=g.current_player.char
+        
+        
+        if (checkWinner()){
+            console.log(checkWinner())
+            document.getElementById('currentPlayer').innerHTML=`Player ${g.current_player.char} won`
 
+        }
+        else{
+            
+            g.changePlayer()
+            document.getElementById('currentPlayer').innerHTML=`Player ${g.current_player.char}'s Turn`
+        }
     }
-    else{
-        console.log(checkWinner())
-        g.changePlayer()
-        document.getElementById('currentPlayer').innerHTML=`Player ${g.current_player.char}'s Turn`
-    }
-    
     }
     
 
 function checkWinner(){
     for (var i=0;i<=3;i++){
-            if (board[0][i]===board[1][i] && board[1][i]===board[2][i]){ 
-                let result = board[0][i]
-                return result
-            }
-            else if (board[i][0]===board[i][1] && board[i][1]===board[i][2]){
-                // #Checking row
-                let result = board[i][0]
-                return result
-            } 
-            else{
-                if (board[0][0]===board[1][1] && board[1][1]===board[2][2]){
         
-                    let result= board[1][1]
-                    return result
-                } 
-                    
-                else if (board[0][2]===board[1][1] && board[2][0]===board[1][1]){
-                    
-                    let result= board[1][1]
-                    return result
+            if (board[0][i]===board[1][i] && board[1][i]===board[2][i] && board[0][i]!=0){ 
+                return board[0][i]
+                
+            }
+            else if (board[i][0]===board[i][1] && board[i][1]===board[i][2] && board[i][0]!=0){
+                // #Checking row
+                return board[i][0]
+                
+            } 
+            
+            else {
+                
+                    if (board[0][0]===board[1][1] && board[1][1]===board[2][2] && board[1][1]!=0){
+                        return board[1][1]
+                        
+                    } 
+                        
+                    else if (board[0][2]===board[1][1] && board[2][0]===board[1][1] && board[1][1]!=0){
+                        return board[1][1]
+                        
+                        }
+                        
+                    else {
+                        continue
                     }
-                    
-                else {
-                    return 0
                 }
             }
-        }
             
-        
-}
+        }
+    
+    
+
+
                 
 
     
